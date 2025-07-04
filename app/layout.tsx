c69fd2b0e8ef3a44de610913522afa56d7eebbe0
YamadaTaiga2025 <yaiga.1380@gmail.com>
2025-07-05 08:37:51 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: '運動会案内サイト',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
